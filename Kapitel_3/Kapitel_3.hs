f639ee07a6b81a47dbab102e37bc2b5d3e67d0fe
Stefan <Stefan.Lobbenmeier@web.de>
2018-10-29 15:10:12 +0100

    Extended kapitel_3.hs

--- Kapitel_3/Kapitel_3.hs
+++ Kapitel_3/Kapitel_3.hs
@@ -1,3 +1,26 @@
+betrag :: (Num a, Ord a) => a -> a
+betrag x
+  | x >= 0 = x
+  | x < 0 = -x
+
+safeHead :: [a] -> Maybe a
+safeHead (x:_) = Just x
+safeHead [] = Nothing
+
+wrap :: (a -> b) -> (Maybe a -> Maybe b)
+wrap f Nothing = Nothing
+wrap f (Just x) = Just (f x)
+
+data Grob = Grob Float deriving Show
+instance Eq Grob where 
+    (==) (Grob a) (Grob b) = (abs (a-b) < 0.01)
+
+a = Grob 42.000
+b = Grob 42.006
+c = Grob 42.012
+
+liste_abc = [a, b, c]
+
 import Data.Char
 
 instance Num Char where 
