c358a5f854443ab017db4273a02a73a29c2fb6fc
Stefan <Stefan.Lobbenmeier@web.de>
2018-11-26 10:45:50 +0100

    erste Selbstkontrollaufgaben bearbeitet

---
+++ Prolog/selbstkontrollaufgaben.pl
@@ -0,0 +1,4 @@
+% 16. o bildet eine bekannte Variable auf eine frische ab. o^-1 bildet diese frische (und damit jetzt bekannte) Variable auf eine bereits bekannte Variable ab.
+% Daher ist o^-1 keine Subsitution
+
+% 17. mgu(t1, t2) = {g(X,Y)/A, h(B,100)/h(Z,W)}
